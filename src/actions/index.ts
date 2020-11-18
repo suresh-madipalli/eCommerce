2171e70c09b5def0127b13f5c551310b3a0698d4
suresh-madipalli <suresh.madipalli@gmail.com>
2020-11-18 11:59:44 +0530

    Add files via upload

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,12 +1,8 @@
-export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
-export const RECEIVE_PRODUCTS = 'RECEIVE_PRODUCTS'
+export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
+export const RECEIVE_PRODUCTS = 'RECEIVE_PRODUCTS';
 
-export const ADD_TO_CART = 'ADD_TO_CART'
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
-
-export const CHECKOUT_REQUEST = 'CHECKOUT_REQUEST'
-export const CHECKOUT_SUCCESS = 'CHECKOUT_SUCCESS'
-export const CHECKOUT_FAILURE = 'CHECKOUT_FAILURE'
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 
 export function getAllProducts() {
   return {
@@ -34,23 +30,4 @@ export function removeFromCart(productId) {
     productId,
   }
 }
-
-export function checkout() {
-  return {
-    type: CHECKOUT_REQUEST,
-  }
-}
-
-export function checkoutSuccess(cart) {
-  return {
-    type: CHECKOUT_SUCCESS,
-    cart,
-  }
-}
-
-export function checkoutFailure(error) {
-  return {
-    type: CHECKOUT_FAILURE,
-    error,
-  }
-}
+ 
